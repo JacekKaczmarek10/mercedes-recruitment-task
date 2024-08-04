dd21390edb820e7a6f9ea4a17c0715ecdd4a683b
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-08-04 16:22:59 +0200

    test: add missing test cases

--- src/test/java/pl/kaczmarek/Recruitment/task/url/UrlServiceTest.java
+++ src/test/java/pl/kaczmarek/Recruitment/task/url/UrlServiceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.ThrowableAssert.catchThrowable;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
