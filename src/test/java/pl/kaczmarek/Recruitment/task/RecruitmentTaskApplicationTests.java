4f54521724ce722077e9fb5f612e9cd38be9a106
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-08-04 17:12:48 +0200

    test: fix test

--- src/test/java/pl/kaczmarek/Recruitment/task/RecruitmentTaskApplicationTests.java
+++ src/test/java/pl/kaczmarek/Recruitment/task/RecruitmentTaskApplicationTests.java
@@ -1,6 +1,9 @@
 package pl.kaczmarek.Recruitment.task;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +11,11 @@ class RecruitmentTaskApplicationTests {
 
 	@Test
 	void contextLoads() {
+		final var applicationClass = RecruitmentTaskApplication.class;
+
+		final var context = SpringApplication.run(applicationClass);
+
+		assertThat(context).isNotNull();
 	}
 
 }
